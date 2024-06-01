4ffe57786b7bc42b0dca6a7fc3d32438af0026a0
mekouar1999 <othman.mekouar99@gmail.com>
2024-06-01 23:53:32 +0100

    test circ

--- front/src/components/Diplomes/diplomes.jsx
+++ front/src/components/Diplomes/diplomes.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { TextField, Button, Box, Container, Typography, MenuItem, LinearProgress } from "@mui/material";
+import { TextField, Button, Box, Container, Typography, MenuItem, LinearProgress, Grid } from "@mui/material";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "./diplome.css";
@@ -42,21 +42,21 @@ const Diplomes = () => {
 
     try {
       const response = await fetch(
-      //  'https://remise-diplomes-back.vercel.app/send-email',
+        // 'https://remise-diplomes-back.vercel.app/send-email',
         'http://localhost:3001/send-email',
-
         {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ etudiant, person1, person2: sanitizedPerson2, person3: sanitizedPerson3, person4: sanitizedPerson4 }),
-        onDownloadProgress: (progressEvent) => {
-          const total = progressEvent.total;
-          const current = progressEvent.loaded;
-          setProgress(Math.round((current / total) * 100));
-        },
-      });
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ etudiant, person1, person2: sanitizedPerson2, person3: sanitizedPerson3, person4: sanitizedPerson4 }),
+          onDownloadProgress: (progressEvent) => {
+            const total = progressEvent.total;
+            const current = progressEvent.loaded;
+            setProgress(Math.round((current / total) * 100));
+          },
+        }
+      );
 
       if (response.ok) {
         const blob = await response.blob();
@@ -105,80 +105,151 @@ const Diplomes = () => {
             </Typography>
             <br/>
 
-            <TextField label="Nom" variant="outlined" margin="normal" fullWidth value={etudiant.nom} onChange={(e) => setEtudiant({ ...etudiant, nom: e.target.value })} required />
-            <TextField label="Prénom" variant="outlined" margin="normal" fullWidth value={etudiant.prenom} onChange={(e) => setEtudiant({ ...etudiant, prenom: e.target.value })} required />
-            <TextField label="CIN" variant="outlined" margin="normal" fullWidth value={etudiant.cin} onChange={(e) => setEtudiant({ ...etudiant, cin: e.target.value })} required />
-            <TextField label="Date de naissance" variant="outlined" margin="normal" fullWidth value={etudiant.dateNaissance} onChange={(e) => setEtudiant({ ...etudiant, dateNaissance: e.target.value })} required />
-            <TextField label="Lieu de naissance" variant="outlined" margin="normal" fullWidth value={etudiant.lieuNaissance} onChange={(e) => setEtudiant({ ...etudiant, lieuNaissance: e.target.value })} required />
-            <TextField select label="Sexe" variant="outlined" margin="normal" fullWidth value={etudiant.sexe} onChange={(e) => setEtudiant({ ...etudiant, sexe: e.target.value })} required>
-              <MenuItem value="M">Masculin</MenuItem>
-              <MenuItem value="F">Féminin</MenuItem>
-            </TextField>
-            <TextField label="Email" variant="outlined" margin="normal" fullWidth value={etudiant.email} onChange={(e) => setEtudiant({ ...etudiant, email: e.target.value })} required />
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <TextField label="Nom" variant="outlined" fullWidth value={etudiant.nom} onChange={(e) => setEtudiant({ ...etudiant, nom: e.target.value })} required />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField label="Prénom" variant="outlined" fullWidth value={etudiant.prenom} onChange={(e) => setEtudiant({ ...etudiant, prenom: e.target.value })} required />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField label="CIN" variant="outlined" fullWidth value={etudiant.cin} onChange={(e) => setEtudiant({ ...etudiant, cin: e.target.value })} required />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField label="Date de naissance" variant="outlined" fullWidth value={etudiant.dateNaissance} onChange={(e) => setEtudiant({ ...etudiant, dateNaissance: e.target.value })} required />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField label="Lieu de naissance" variant="outlined" fullWidth value={etudiant.lieuNaissance} onChange={(e) => setEtudiant({ ...etudiant, lieuNaissance: e.target.value })} required />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField select label="Sexe" variant="outlined" fullWidth value={etudiant.sexe} onChange={(e) => setEtudiant({ ...etudiant, sexe: e.target.value })} required>
+                  <MenuItem value="M">Masculin</MenuItem>
+                  <MenuItem value="F">Féminin</MenuItem>
+                </TextField>
+              </Grid>
+              <Grid item xs={12}>
+                <TextField label="Email" variant="outlined" fullWidth value={etudiant.email} onChange={(e) => setEtudiant({ ...etudiant, email: e.target.value })} required />
+              </Grid>
+            </Grid>
 
             <Typography variant="h6" gutterBottom>
               Invité 1
             </Typography>
-            <TextField label="Nom" variant="outlined" margin="normal" fullWidth value={person1.nom} onChange={(e) => setPerson1({ ...person1, nom: e.target.value })} required />
-            <TextField label="Prénom" variant="outlined" margin="normal" fullWidth value={person1.prenom} onChange={(e) => setPerson1({ ...person1, prenom: e.target.value })} required />
-            <TextField label="CIN" variant="outlined" margin="normal" fullWidth value={person1.cin} onChange={(e) => setPerson1({ ...person1, cin: e.target.value })} required />
-            <TextField label="Date de naissance" variant="outlined" margin="normal" fullWidth value={person1.dateNaissance} onChange={(e) => setPerson1({ ...person1, dateNaissance: e.target.value })} required />
-            <TextField label="Lieu de naissance" variant="outlined" margin="normal" fullWidth value={person1.lieuNaissance} onChange={(e) => setPerson1({ ...person1, lieuNaissance: e.target.value })} required />
-            <TextField select label="Sexe" variant="outlined" margin="normal" fullWidth value={person1.sexe} onChange={(e) => setPerson1({ ...person1, sexe: e.target.value })} required>
-              <MenuItem value="M">Masculin</MenuItem>
-              <MenuItem value="F">Féminin</MenuItem>
-            </TextField>
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <TextField label="Nom" variant="outlined" fullWidth value={person1.nom} onChange={(e) => setPerson1({ ...person1, nom: e.target.value })} required />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField label="Prénom" variant="outlined" fullWidth value={person1.prenom} onChange={(e) => setPerson1({ ...person1, prenom: e.target.value })} required />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField label="CIN" variant="outlined" fullWidth value={person1.cin} onChange={(e) => setPerson1({ ...person1, cin: e.target.value })} required />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField label="Date de naissance" variant="outlined" fullWidth value={person1.dateNaissance} onChange={(e) => setPerson1({ ...person1, dateNaissance: e.target.value })} required />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField label="Lieu de naissance" variant="outlined" fullWidth value={person1.lieuNaissance} onChange={(e) => setPerson1({ ...person1, lieuNaissance: e.target.value })} required />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField select label="Sexe" variant="outlined" fullWidth value={person1.sexe} onChange={(e) => setPerson1({ ...person1, sexe: e.target.value })} required>
+                  <MenuItem value="M">Masculin</MenuItem>
+                  <MenuItem value="F">Féminin</MenuItem>
+                </TextField>
+              </Grid>
+            </Grid>
 
             <Typography variant="h6" gutterBottom>
               Invité 2
             </Typography>
-            <TextField label="Nom" variant="outlined" margin="normal" fullWidth value={person2.nom} onChange={(e) => setPerson2({ ...person2, nom: e.target.value })} />
-            <TextField label="Prénom" variant="outlined" margin="normal" fullWidth value={person2.prenom} onChange={(e) => setPerson2({ ...person2, prenom: e.target.value })} />
-            <TextField label="CIN" variant="outlined" margin="normal" fullWidth value={
-person2.cin} onChange={(e) => setPerson2({ ...person2, cin: e.target.value })} />
-<TextField label="Date de naissance" variant="outlined" margin="normal" fullWidth value={person2.dateNaissance} onChange={(e) => setPerson2({ ...person2, dateNaissance: e.target.value })} />
-<TextField label="Lieu de naissance" variant="outlined" margin="normal" fullWidth value={person2.lieuNaissance} onChange={(e) => setPerson2({ ...person2, lieuNaissance: e.target.value })} />
-<TextField select label="Sexe" variant="outlined" margin="normal" fullWidth value={person2.sexe} onChange={(e) => setPerson2({ ...person2, sexe: e.target.value })}>
-  <MenuItem value="M">Masculin</MenuItem>
-  <MenuItem value="F">Féminin</MenuItem>
-</TextField>
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <TextField label="Nom" variant="outlined" fullWidth value={person2.nom} onChange={(e) => setPerson2({ ...person2, nom: e.target.value })} />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField label="Prénom" variant="outlined" fullWidth value={person2.prenom} onChange={(e) => setPerson2({ ...person2, prenom: e.target.value })} />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField label="CIN" variant="outlined" fullWidth value={person2.cin} onChange={(e) => setPerson2({ ...person2, cin: e.target.value })} />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField label="Date de naissance" variant="outlined" fullWidth value={person2.dateNaissance} onChange={(e) => setPerson2({ ...person2, dateNaissance: e.target.value })} />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField label="Lieu de naissance" variant="outlined" fullWidth value={person2.lieuNaissance} onChange={(e) => setPerson2({ ...person2, lieuNaissance: e.target.value })} />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField select label="Sexe" variant="outlined" fullWidth value={person2.sexe} onChange={(e) => setPerson2({ ...person2, sexe: e.target.value })}>
+                  <MenuItem value="M">Masculin</MenuItem>
+                  <MenuItem value="F">Féminin</MenuItem>
+                </TextField>
+              </Grid>
+            </Grid>
 
-<Typography variant="h6" gutterBottom>
-  Invité 3
-</Typography>
-<TextField label="Nom" variant="outlined" margin="normal" fullWidth value={person3.nom} onChange={(e) => setPerson3({ ...person3, nom: e.target.value })} />
-<TextField label="Prénom" variant="outlined" margin="normal" fullWidth value={person3.prenom} onChange={(e) => setPerson3({ ...person3, prenom: e.target.value })} />
-<TextField label="CIN" variant="outlined" margin="normal" fullWidth value={person3.cin} onChange={(e) => setPerson3({ ...person3, cin: e.target.value })} />
-<TextField label="Date de naissance" variant="outlined" margin="normal" fullWidth value={person3.dateNaissance} onChange={(e) => setPerson3({ ...person3, dateNaissance: e.target.value })} />
-<TextField label="Lieu de naissance" variant="outlined" margin="normal" fullWidth value={person3.lieuNaissance} onChange={(e) => setPerson3({ ...person3, lieuNaissance: e.target.value })} />
-<TextField select label="Sexe" variant="outlined" margin="normal" fullWidth value={person3.sexe} onChange={(e) => setPerson3({ ...person3, sexe: e.target.value })}>
-  <MenuItem value="M">Masculin</MenuItem>
-  <MenuItem value="F">Féminin</MenuItem>
-</TextField>
+            <Typography variant="h6" gutterBottom>
+              Invité 3
+            </Typography>
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <TextField label="Nom" variant="outlined" fullWidth value={person3.nom} onChange={(e) => setPerson3({ ...person3, nom: e.target.value })} />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField label="Prénom" variant="outlined" fullWidth value={person3.prenom} onChange={(e) => setPerson3({ ...person3, prenom: e.target.value })} />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField label="CIN" variant="outlined" fullWidth value={person3.cin} onChange={(e) => setPerson3({ ...person3, cin: e.target.value })} />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField label="Date de naissance" variant="outlined" fullWidth value={person3.dateNaissance} onChange={(e) => setPerson3({ ...person3, dateNaissance: e.target.value })} />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField label="Lieu de naissance" variant="outlined" fullWidth value={person3.lieuNaissance} onChange={(e) => setPerson3({ ...person3, lieuNaissance: e.target.value })} />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField select label="Sexe" variant="outlined" fullWidth value={person3.sexe} onChange={(e) => setPerson3({ ...person3, sexe: e.target.value })}>
+                  <MenuItem value="M">Masculin</MenuItem>
+                  <MenuItem value="F">Féminin</MenuItem>
+                </TextField>
+              </Grid>
+            </Grid>
 
-<Typography variant="h6" gutterBottom>
-  Invité 4
-</Typography>
-<TextField label="Nom" variant="outlined" margin="normal" fullWidth value={person4.nom} onChange={(e) => setPerson4({ ...person4, nom: e.target.value })} />
-<TextField label="Prénom" variant="outlined" margin="normal" fullWidth value={person4.prenom} onChange={(e) => setPerson4({ ...person4, prenom: e.target.value })} />
-<TextField label="CIN" variant="outlined" margin="normal" fullWidth value={person4.cin} onChange={(e) => setPerson4({ ...person4, cin: e.target.value })} />
-<TextField label="Date de naissance" variant="outlined" margin="normal" fullWidth value={person4.dateNaissance} onChange={(e) => setPerson4({ ...person4, dateNaissance: e.target.value })} />
-<TextField label="Lieu de naissance" variant="outlined" margin="normal" fullWidth value={person4.lieuNaissance} onChange={(e) => setPerson4({ ...person4, lieuNaissance: e.target.value })} />
-<TextField select label="Sexe" variant="outlined" margin="normal" fullWidth value={person4.sexe} onChange={(e) => setPerson4({ ...person4, sexe: e.target.value })}>
-  <MenuItem value="M">Masculin</MenuItem>
-  <MenuItem value="F">Féminin</MenuItem>
-</TextField>
+            <Typography variant="h6" gutterBottom>
+              Invité 4
+            </Typography>
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <TextField label="Nom" variant="outlined" fullWidth value={person4.nom} onChange={(e) => setPerson4({ ...person4, nom: e.target.value })} />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField label="Prénom" variant="outlined" fullWidth value={person4.prenom} onChange={(e) => setPerson4({ ...person4, prenom: e.target.value })} />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField label="CIN" variant="outlined" fullWidth value={person4.cin} onChange={(e) => setPerson4({ ...person4, cin: e.target.value })} />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField label="Date de naissance" variant="outlined" fullWidth value={person4.dateNaissance} onChange={(e) => setPerson4({ ...person4, dateNaissance: e.target.value })} />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField label="Lieu de naissance" variant="outlined" fullWidth value={person4.lieuNaissance} onChange={(e) => setPerson4({ ...person4, lieuNaissance: e.target.value })} />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField select label="Sexe" variant="outlined" fullWidth value={person4.sexe} onChange={(e) => setPerson4({ ...person4, sexe: e.target.value })}>
+                  <MenuItem value="M">Masculin</MenuItem>
+                  <MenuItem value="F">Féminin</MenuItem>
+                </TextField>
+              </Grid>
+            </Grid>
 
-<Button type="submit" variant="contained" color="primary" sx={{ mt: 3 }} disabled={loading}>
-  {loading ? 'Envoi en cours...' : 'Envoyer l\'invitation'}
-</Button>
-{loading && <LinearProgress variant="determinate" value={progress} />}
-</form>
-</Box>
-</Container>
-<ToastContainer />
-</>
-);
+            <Button type="submit" variant="contained" color="primary" sx={{ mt: 3 }} disabled={loading}>
+              {loading ? 'Envoi en cours...' : 'Envoyer l\'invitation'}
+            </Button>
+            {loading && <LinearProgress variant="determinate" value={progress} />}
+          </form>
+        </Box>
+      </Container>
+      <ToastContainer />
+    </>
+  );
 };
 
 export default Diplomes;
